624a85c6082f1fda41e080655db8b9faefd5df88
Omar D'agostino <omardagostino@gmail.com>
2023-08-22 05:47:42 -0300

    version 1.0.5

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import express from 'express';
-// import bodyParser from 'body-parser';
 import apiRouter from './routes/users.routes.js';
 
 const app = express();
 const PORT = 8080;
 
-// app.use(bodyParser.json());
 app.use(express.json());
 
 app.use('/api', apiRouter);
